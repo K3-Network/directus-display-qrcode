385738a36000dd0f025b0eb19165ba16ff443ae8
Nicolas Mierbach <nicolas@k3.network>
2021-09-30 13:55:52 +0200

    Changed toggle function from arrow to normal funtion to prevent 'this' overwriting

--- src/display.vue
+++ src/display.vue
@@ -1,22 +1,21 @@
 <template>
-  <div 
-    @mouseover="isOpen = true"
-    @mouseleave="isOpen = false"
-    class="qrcode"
-    >
-    <v-menu
-      delay="200"
-      show-arrow
-      :disabled="value.length === 0"
-      >
+  <div @mouseover="isOpen = true" @mouseleave="isOpen = false" class="qrcode">
+    <v-menu delay="200" show-arrow :disabled="value.length === 0">
       <template #activator="{ toggle }">
         <span class="toggle" @click.stop="toggle">
           <span class="label">
-            <v-icon class="icon" name="qr_code_scanner" small /> {{ value }} 
+            <v-icon class="icon" name="qr_code_scanner" small /> {{ value }}
           </span>
         </span>
-		  </template>
-      <qrcode-vue class="qr" :value="value" background="rgba(0,0,0,0)" render-as="svg" :size="120" level="H" />
+      </template>
+      <qrcode-vue
+        class="qr"
+        :value="value"
+        background="rgba(0,0,0,0)"
+        render-as="svg"
+        :size="120"
+        level="H"
+      />
     </v-menu>
   </div>
 </template>
@@ -33,52 +32,51 @@ export default {
   },
   data() {
     return {
-      isOpen: false
-    }
+      isOpen: false,
+    };
   },
   methods: {
-    toggle: () => {
-      this.isOpen = !this.isOpen
-    }
+    toggle: function () {
+      this.isOpen = !this.isOpen;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .v-icon {
-	--v-icon-color: var(--foreground-subdued);
+  --v-icon-color: var(--foreground-subdued);
 }
 .toggle {
-	position: relative;
-	&::before {
-		position: absolute;
-		top: -6px;
-		left: -6px;
-		z-index: 1;
-		width: calc(100% + 12px);
-		height: calc(100% + 12px);
-		background-color: var(--background-normal);
-		border-radius: var(--border-radius);
-		opacity: 0;
-		transition: opacity var(--fast) var(--transition);
-		content: '';
-	}
-	.label {
-		position: relative;
-		z-index: 2;
-	}
-	&:not(.subdued):hover::before {
-		opacity: 1;
-	}
-	&:not(.subdued):active::before {
-		background-color: var(--background-normal-alt);
-	}
+  position: relative;
+  &::before {
+    position: absolute;
+    top: -6px;
+    left: -6px;
+    z-index: 1;
+    width: calc(100% + 12px);
+    height: calc(100% + 12px);
+    background-color: var(--background-normal);
+    border-radius: var(--border-radius);
+    opacity: 0;
+    transition: opacity var(--fast) var(--transition);
+    content: "";
+  }
+  .label {
+    position: relative;
+    z-index: 2;
+  }
+  &:not(.subdued):hover::before {
+    opacity: 1;
+  }
+  &:not(.subdued):active::before {
+    background-color: var(--background-normal-alt);
+  }
 }
 
 .qr {
-    padding: .2em;
-    margin-top: .4em;
-    margin-bottom: 0;
+  padding: 0.2em;
+  margin-top: 0.4em;
+  margin-bottom: 0;
 }
-
 </style>
